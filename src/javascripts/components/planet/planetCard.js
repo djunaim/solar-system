1628bc204d5c2bdde08d8c7c667e85a2833d2b5a
Monica Djunaidi <modjun12@gmail.com>
2019-10-12 13:22:35 -0500

    added search box

--- src/javascripts/components/planet/planetCard.js
+++ src/javascripts/components/planet/planetCard.js
@@ -11,19 +11,56 @@ const planetCard = () => {
     domString += `
       <div class="col-4">
         <div class="card">
-          <div class="card-body">
-            <h5 class="card-title d-flex align-content-center">${planet.name}</h5>
-          </div>
-        </div>
-      </div>  
+        <h4 id="planetName" class="d-flex justify-content-center">${planet.name}</h4>
+            <div class="card-body">
+              <img src="${planet.imageUrl}" class="card-img-top d-none">
+              <p class="card-text">${planet.description}</p>
+              <p class="card-text">${planet.isGasPlanet}</p>
+              <p class="card-text">${planet.numberOfMoons}</p>
+              <p class="card-text">${planet.nameOfLargestMoon}</p>
+            </div>
+        </div> 
+      </div>   
     `;
   }
   domString += '</div>';
   utilities.printToDOM('planetarium', domString);
 };
 
-const searchFilter = () => {
-  $(document).ready()
+const hideInfo = () => {
+  $('.card-text').hide();
 };
 
-export default { planetCard };
+const hoverName = (e) => {
+  $(e.target).find('#h4').toggleClass('d-none');
+};
+
+const hideName = () => {
+  $('.card').hover(hoverName);
+};
+
+const hoverImg = (e) => {
+  $(e.target).find('img').toggleClass('d-none');
+};
+
+const showImg = () => {
+  $('.card').hover(hoverImg);
+};
+
+// const results = () => {
+//   $('#planetSearch').on('keyup', () => {
+//     const input = $(this).val().toLowerCase();
+//     $('#planetarium').filter((word) => {
+//       if (input.indexOf(word.toLowerCase) !== -1) {
+//         planetCard();
+//       }
+//     });
+//   });
+// };
+
+const hoverEvent = () => {
+  hideName();
+  showImg();
+};
+
+export default { planetCard, hideInfo, hoverEvent };
