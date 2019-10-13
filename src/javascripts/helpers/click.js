5c3d00edc8795f20bc11958dd2ac239d6a6af9f3
Monica Djunaidi <modjun12@gmail.com>
2019-10-13 15:12:14 -0500

    wip

--- src/javascripts/helpers/click.js
+++ src/javascripts/helpers/click.js
@@ -1,16 +1,20 @@
 import $ from 'jquery';
 import planetCard from '../components/planet/planetCard';
 
-const cardClick = (e) => {
+const cardClick = () => {
   planetCard.planetCard();
   const cardGroup = $('.card');
   for (let i = 0; i < cardGroup.length; i += 1) {
-    const planetID = $(e.target).find('.card').html();
     const singleCard = cardGroup[i];
-    if (singleCard.style.display === 'block' && singleCard.name === planetID) {
-      singleCard.style.display = 'none';
-    } else if (singleCard.style.display === 'none' && singleCard.name === planetID) {
-      singleCard.style.display = 'block';
+    console.log(singleCard);
+    const planetID = singleCard.children[0].innerHTML;
+    console.log(planetID);
+    const cardName = cardGroup.find('#planetName').html();
+    console.log(cardName);
+    if (singleCard.style.visibility === 'visible' && cardName === planetID) {
+      singleCard.show();
+    } else if (singleCard.style.visibility === 'visible' && cardName !== planetID) {
+      singleCard.hide();
     }
   }
 };
