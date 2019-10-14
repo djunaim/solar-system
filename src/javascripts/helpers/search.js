8101d2c04fefb080b2db67fe34718c155ae11a8e
Monica Djunaidi <modjun12@gmail.com>
2019-10-14 08:45:54 -0500

    commented out search function

--- src/javascripts/helpers/search.js
+++ src/javascripts/helpers/search.js
@@ -1,18 +1,18 @@
-import $ from 'jquery';
-import planetData from './data/planetData';
-import utilities from './utilities';
+// import $ from 'jquery';
+// import planetData from './data/planetData';
+// import utilities from './utilities';
 
-const results = () => {
-  $('#planetSearch').on('keyup', () => {
-    const newPlanets = '';
-    const input = $(this).val().toLowerCase();
-    $('#planetarium').filter((word) => {
-      if (planetData.planetData().indexOf(word.toLowerCase) === input) {
-        newPlanets.push();
-        utilities.printToDOM('planetarium', newPlanets);
-      }
-    });
-  });
-};
+// const results = () => {
+//   $('#planetSearch').on('keyup', () => {
+//     const newPlanets = '';
+//     const input = $(this).val().toLowerCase();
+//     $('#planetarium').filter((word) => {
+//       if (planetData.planetData().indexOf(word.toLowerCase) === input) {
+//         newPlanets.push();
+//         utilities.printToDOM('planetarium', newPlanets);
+//       }
+//     });
+//   });
+// };
 
-export default { results };
+// export default { results };
