975dc8ca576b8ef6ec31baef61a542c2bec5e97e
Monica Djunaidi <modjun12@gmail.com>
2019-10-14 10:40:03 -0500

    wip

--- src/javascripts/helpers/click.js
+++ src/javascripts/helpers/click.js
@@ -1,20 +1,26 @@
 import $ from 'jquery';
 import planetCard from '../components/planet/planetCard';
+import planetData from './data/planetData';
+import utilities from './utilities';
 
 const cardClick = () => {
   planetCard.planetCard();
+  const allPlanets = planetData.getPlanet();
   const cardGroup = $('.card');
   for (let i = 0; i < cardGroup.length; i += 1) {
     const singleCard = cardGroup[i];
-    console.log(singleCard);
-    const planetID = singleCard.children[0].innerHTML;
+    const planetID = $('.card').find('.planetName').html();
     console.log(planetID);
-    const cardName = cardGroup.find('#planetName').html();
-    console.log(cardName);
-    if (singleCard.style.visibility === 'visible' && cardName === planetID) {
-      singleCard.show();
-    } else if (singleCard.style.visibility === 'visible' && cardName !== planetID) {
-      singleCard.hide();
+    for (let j = 0; j < allPlanets.length; j += 1) {
+      const singlePlanet = allPlanets[j];
+      console.log(singlePlanet);
+      const cardName = cardGroup.find('.planetName').html();
+      console.log(cardName);
+      if (planetID === singlePlanet.name) {
+        utilities.printToDOM('planetarium', singleCard);
+      // } else if (planetID !== singlePlanet.name) {
+      //   singleCard.hide();
+      }
     }
   }
 };
